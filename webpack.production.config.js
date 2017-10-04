a3c6c006831c6d4a612c60ba2472e2f34ec9473f
Brandon Sherette <brandonsherette@gmail.com>
2017-10-04 13:16:09 -0700

    Updated build.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,6 +3,20 @@ const config = require('./config.js');
 const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const dependencies = require('./package.json').dependencies;
+const externals = {};
+const allDependencies = Object.assign({}, dependencies, {
+  /* add css dependencies */
+});
+
+for (var extName in allDependencies) {
+  externals[extName] = {
+    root: (extName === 'react') ? 'React' : extName,
+    commonjs: extName,
+    commonjs2: extName,
+    amd: extName,
+  }
+};
 
 module.exports = {
   entry: './src/app/' + config.libraryName + '/index.js',
@@ -14,74 +28,7 @@ module.exports = {
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
-  externals: {
-    'classnames': {
-      root: 'classnames',
-      commonjs2: 'classnames',
-      commonjs: 'classnames',
-      amd: 'classnames'
-    },
-    'prop-types': {
-      root: 'prop-types',
-      commonjs2: 'prop-types',
-      commonjs: 'prop-types',
-      amd: 'prop-types'
-    },
-    'rc-slider': {
-      root: 'rc-slider',
-      commonjs2: 'rc-slider',
-      commonjs: 'rc-slider',
-      amd: 'rc-slider'
-    },
-    'react': {
-      root: 'React',
-      commonjs2: 'react',
-      commonjs: 'react',
-      amd: 'react',
-    },
-    'react-addons-css-transition-group': {
-      root: 'react-addons-css-transition-group',
-      commonjs2: 'react-addons-css-transition-group',
-      commonjs: 'react-addons-css-transition-group',
-      amd: 'react-addons-css-transition-group',
-    },
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs2: 'react-dom',
-      commonjs: 'react-dom',
-      amd: 'react-dom',
-    },
-    'react-redux': {
-      root: 'react-redux',
-      commonjs2: 'react-redux',
-      commonjs: 'react-redux',
-      amd: 'react-redux',
-    },
-    'react-select': {
-      root: 'react-select',
-      commonjs2: 'react-select',
-      commonjs: 'react-select',
-      amd: 'react-select'
-    },
-    'redux': {
-      root: 'redux',
-      commonjs2: 'redux',
-      commonjs: 'redux',
-      amd: 'redux'
-    },
-    'redux-form': {
-      root: 'redux-form',
-      commonjs2: 'redux-form',
-      commonjs: 'redux-form',
-      amd: 'redux-form'
-    },
-    'redux-logger': {
-      root: 'logger',
-      commonjs2: 'redux-logger',
-      commonjs: 'redux-logger',
-      amd: 'redux-logger',
-    },
-  },
+  externals,
   module: {
     loaders: [
       {
